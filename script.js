6381fd8f2fbc1ca762979d8da78f2266484cfd42
bar <barkeila@gmail.com>
2022-11-18 23:51:04 +0200

    paint

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const game = {
 
 
 function randomWord (){
-    let randomNum = Math.ceil(Math.random()*20);
+    let randomNum = Math.ceil(Math.random()*20)-1;
     return wordList[randomNum];
 }
 
@@ -22,15 +22,40 @@ function checkUserInput (){
 }
 
 function setLetter(letter) {
+    let cell=""
     if (game.numOfCell==6){
         return;
     }
     else {
-        let c = letter;
-        let box = document.getElementById("letter"+game.numOfRow+game.numOfCell);
-        box.innerText = c;
+        if (game.numOfCell == 5) {
+            cell = terminalLetter(letter)
+        }
+        else {
+            cell = letter;
+        }
+        let box = document.getElementById("letter" + game.numOfRow + game.numOfCell);
+        box.innerText = cell;
         game.numOfCell++;
     }
+}
+
+function terminalLetter (letter){
+    if (letter=="מ"){
+        letter="ם"
+    }
+    if (letter=="נ"){
+        letter="ן"
+    }
+    if (letter=="כ"){
+        letter="ך"
+    }
+    if (letter=="צ"){
+        letter="ץ"
+    }
+    if (letter=="פ"){
+        letter="ף"
+    }
+    return letter;
 }
 function deleteLetter() {
     let cell = game.numOfCell-1;
@@ -64,10 +89,16 @@ function checkGuss () {
             index++;
         }
         if (checkIfWordInStock(word)) {
-
+            paintCellAndKey(word);
             if (word == game.secWord) {
-                alert("good");
-            } else alert("no")
+                alert("you win");
+            } else{
+                game.numOfRow++;
+                game.numOfCell=1;
+                if (game.numOfRow==7){
+                    alert("you lose")
+                }
+            }
         } else alert("no in stock")
     }
 }
@@ -82,12 +113,33 @@ function checkIfWordInStock (word){
     }
 }
 
-function paintCell(){
-
+function paintCellAndKey(word){
+    let row=game.numOfRow;
+    for (let i=1;i<6;i++) {
+        let letter2 = game.secWord.charAt(i-1);
+        for (let j = 1; j<6; j++) {
+            let letter1 = word.charAt(j - 1)
+            if ((letter1 == letter2) && (i == j)) {
+                document.getElementById("letter" + row + i).style.background = "green";
+                document.getElementById(letter1).style.background = "green";
+            }
+            if (document.getElementById("letter" + row + j).style.background != "green") {
+                if ((letter1 == letter2) && (i != j)) {
+                    document.getElementById("letter" + row + j).style.background = "yellow";
+                    document.getElementById(letter1).style.background = "yellow";
+                }
+            }
+            if ((document.getElementById("letter" + row + j).style.background != "green")&&(document.getElementById("letter" + row + j).style.background != "yellow"))
+            {
+                if ((letter1 != letter2)) {
+                    document.getElementById("letter" + row + j).style.background = "gray";
+                    document.getElementById(letter1).style.background = "gray";
+                }
+            }
+        }
+    }
 }
-function paintKeyBord(){
 
-}
 
 
 
